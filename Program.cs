c6bda9127ae653cecd55494d19b9e054f251fd37
degerahmet <adeger34@gmail.com>
2025-01-12 13:44:51 -0800

    hw done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace DEV120 {
+    class Fruit{
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public Fruit(int weight, string name){
+            Weight = weight;
+            Name = name;
+        }
+
+        internal void PrintFruits(){
+            Console.WriteLine($"Name: {Name}, Weight: {Weight}");
+        }
+    }
+}
---
+++ Runner.cs
@@ -0,0 +1,12 @@
+
+// Import Fruit
+using System;
+using HW1;
+
+Fruit apple = new Fruit(10, "Apple");
+Fruit banana = new Fruit(20, "Banana");
+Fruit strawberry = new Fruit(5, "Strawberry");
+
+apple.PrintFruits();
+banana.PrintFruits();
+strawberry.PrintFruits();
